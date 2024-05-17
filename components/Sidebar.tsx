3063801f174ad92369f9dc15905904ea1f806305
Tony Nguyen <tonyqnguyen2003@gmail.com>
2024-05-17 18:56:32 -0400

    Added styling and minor components to main content

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import { BiSearch } from "react-icons/bi";
 import { HiHome } from "react-icons/hi";
 import Box from "./Box";
 import SidebarItem from "./SidebarItem";
+import Library from "./Library";
 
 interface SidebarProps { 
     children: React.ReactNode;
@@ -67,7 +68,9 @@ const Sidebar: React.FC<SidebarProps> = ({
             </Box>
 
             <Box className="overflow-y-auto h-full">
-                Song Library
+                <Library>
+                        
+                </Library>            
             </Box>
         </div>
         <main className="h-full flex-1 overflox-y-auto py-2">
