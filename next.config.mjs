b73fb81a66723fc1912be0a7e4e8a0136ac44e62
Tony Nguyen <tonyqnguyen2003@gmail.com>
2024-06-12 13:58:30 -0400

    added song image tiles to the grid

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: { 
+        domains: [
+            "zjaqusbgkwowrmapbeke.supabase.co"
+        ]
+    }
+};
 
 export default nextConfig;
--- types.ts
+++ types.ts
@@ -1,5 +1,14 @@
 import Stripe from "stripe";
 
+export interface Song { 
+    id: string;
+    user_id: string;
+    author: string;
+    title: string;
+    song_path: string;
+    image_path: string;
+};
+
 /* Interfaces Needed for useUser hook */
 export interface UserDetails { 
     id: string;
