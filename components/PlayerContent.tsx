f4fbe4afe28d8bc0b56c0cbe8d9bfecf30fa574d
Tony Nguyen <tonyqnguyen2003@gmail.com>
2024-06-13 14:59:31 -0400

    Added sound funtionality to player modal

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -1,13 +1,15 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { Song } from "@/types";
-import MediaItem from "./MediaItem";
-import LikeButton from "./LikeButton";
 import { BsPauseFill, BsPlayFill } from "react-icons/bs";
 import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
 import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
+import MediaItem from "./MediaItem";
+import LikeButton from "./LikeButton";
 import Slider from "./Slider";
-
+import usePlayer from "@/hooks/usePlayer";
+import useSound from "use-sound";
 
 interface PlayerContentProps { 
     song: Song;
@@ -18,9 +20,83 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
     song,
     songUrl
 }) => {
+    const player = usePlayer();
+    const [volume, setVolume] = useState(1);
+    const [isPlaying, setIsPlaying] = useState(false);
 
-    const Icon = true ? BsPauseFill : BsPlayFill;
-    const VolumeIcon = true ? HiSpeakerXMark : HiSpeakerWave;
+    const Icon = isPlaying ? BsPauseFill : BsPlayFill;
+    const VolumeIcon = volume === 0 ? HiSpeakerXMark : HiSpeakerWave;
+
+    const onPlayNext = () => {
+        if (player.ids.length === 0) { 
+            return;
+        }
+
+        const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+        const nextSong = player.ids[currentIndex + 1];
+
+        /* Check if there is a next song */
+        if (!nextSong) { 
+            return player.setId(player.ids[0]); // resets the playlist
+        }
+
+        player.setId(nextSong);
+    };
+
+    const onPlayPrevious = () => {
+        if (player.ids.length === 0) { 
+            return;
+        }
+
+        const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+        const previousSong = player.ids[currentIndex - 1];
+
+        /* Check if there is a previous song */
+        if (!previousSong) { 
+            return player.setId(player.ids[player.ids.length - 1]); // resets the playlist
+        }
+
+        player.setId(previousSong);
+    };
+
+    const [play, { pause, sound }] = useSound(
+        songUrl,
+        {
+            volume: volume,
+            onplay: () => setIsPlaying(true),
+            onend: () => {
+                setIsPlaying(false);
+                onPlayNext();
+            },
+            onpause: () => setIsPlaying(false),
+            format: ['mp3']
+        }
+    );
+
+    /* Automatically Plays Song */
+    useEffect(() => {
+        sound?.play();
+
+        return() => {
+            sound?.unload();
+        }
+    }, [sound]);
+
+    const handlePlay = () => { 
+        if (!isPlaying) { 
+            play();
+        } else { 
+            pause();
+        }
+    };
+
+    const toggleMute = () => {
+        if (volume === 0) { 
+            setVolume(1);
+        } else { 
+            setVolume(0);
+        }
+    };
 
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 h-full">
@@ -33,7 +109,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
 
         <div className="flex md:hidden col-auto w-full justify-end items-center">
             <div
-                onClick={() => {}}
+                onClick={handlePlay}
                 className="
                     h-10
                     w-10
@@ -62,7 +138,8 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
                 gap-x-6
             "
         >
-            <AiFillStepBackward 
+            <AiFillStepBackward
+            onClick={onPlayPrevious} 
                 size={30}
                 className="
                     text-neutral-400
@@ -73,7 +150,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
             />
 
             <div
-                onClick={() => {}}
+                onClick={handlePlay}
                 className="
                     flex
                     items-center
@@ -89,7 +166,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
                 <Icon size={30} className="text-black"/>
             </div>
             <AiFillStepForward 
-                onClick={() => {}}
+                onClick={onPlayNext}
                 size={30}
                 className="
                     text-neutral-400
@@ -103,13 +180,17 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
         <div className="hidden md:flex w-full justify-end pr-2">
             <div className="flex items-center gap-x-2 w-[120px]"> 
                 <VolumeIcon 
-                    onClick={() => {}}
+                    onClick={toggleMute}
                     className="cursor-pointer"
                     size={34}
                 />
-                <Slider />
+                <Slider 
+                    value={volume}
+                    onChange={(value) => setVolume(value)}
+                />
             </div>
         </div>
+
     </div>
   )
 }
